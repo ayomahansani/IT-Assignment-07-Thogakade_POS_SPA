3dfb8a8e664fea19012d3f75a7528428c495328f
ayomahansani <ayohansi17@gmail.com>
2024-05-02 22:16:56 +0530

    write js for home page

--- js/new.js
+++ js/new.js
@@ -1,11 +1,23 @@
 // when click navbar
+
 $('#customer-section').css({display: 'none'});
 $('#item-section').css({display: 'none'});
 $('#order-section').css({display: 'none'});
 
 
+/* start home nav management */
+$('#nav-home').on("click", function () {
+    $('#home-section').css({display: 'block'});
+    $('#customer-section').css({display: 'none'});
+    $('#item-section').css({display: 'none'});
+    $('#order-section').css({display: 'none'});
+});
+/* end home nav management */
+
+
 /* start customers nav management */
 $('#nav-customers').on("click", function () {
+    $('#home-section').css({display: 'none'});
     $('#customer-section').css({display: 'block'});
     $('#item-section').css({display: 'none'});
     $('#order-section').css({display: 'none'});
@@ -15,6 +27,7 @@ $('#nav-customers').on("click", function () {
 
 /* start items nav management */
 $('#nav-items').on("click", function () {
+    $('#home-section').css({display: 'none'});
     $('#customer-section').css({display: 'none'});
     $('#item-section').css({display: 'block'});
     $('#order-section').css({display: 'none'});
@@ -24,6 +37,7 @@ $('#nav-items').on("click", function () {
 
 /* start items nav management */
 $('#nav-orders').on("click", function () {
+    $('#home-section').css({display: 'none'});
     $('#customer-section').css({display: 'none'});
     $('#item-section').css({display: 'none'});
     $('#order-section').css({display: 'block'});
