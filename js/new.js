2cfac904a23a3f2a4d30901816f26861e80e61d5
ayomahansani <ayohansi17@gmail.com>
2024-05-02 21:50:27 +0530

    write js for customer manage page

---
+++ js/new.js
@@ -0,0 +1,12 @@
+// when click navbar
+$('#customer-section').css({display: 'none'});
+$('#item-section').css({display: 'none'});
+$('#order-section').css({display: 'none'});
+
+/* start customers nav management */
+$('#nav-customers').on("click", function () {
+    $('#customer-section').css({display: 'block'});
+    $('#item-section').css({display: 'none'});
+    $('#order-section').css({display: 'none'});
+});
+/* end customers nav management */
