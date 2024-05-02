771ef8615f0cbc4563a9626aeb343db5603e25aa
ayomahansani <ayohansi17@gmail.com>
2024-05-02 21:52:18 +0530

    write js for order manage page

--- js/new.js
+++ js/new.js
@@ -3,6 +3,7 @@ $('#customer-section').css({display: 'none'});
 $('#item-section').css({display: 'none'});
 $('#order-section').css({display: 'none'});
 
+
 /* start customers nav management */
 $('#nav-customers').on("click", function () {
     $('#customer-section').css({display: 'block'});
@@ -11,10 +12,20 @@ $('#nav-customers').on("click", function () {
 });
 /* end customers nav management */
 
+
 /* start items nav management */
 $('#nav-items').on("click", function () {
     $('#customer-section').css({display: 'none'});
     $('#item-section').css({display: 'block'});
     $('#order-section').css({display: 'none'});
+});
+/* end items nav management */
+
+
+/* start items nav management */
+$('#nav-orders').on("click", function () {
+    $('#customer-section').css({display: 'none'});
+    $('#item-section').css({display: 'none'});
+    $('#order-section').css({display: 'block'});
 });
 /* end items nav management */
